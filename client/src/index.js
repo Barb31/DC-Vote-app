f35f3c9a41d8547a5f17cfee51a9168a0f61fb1f
Barb31 <barbaraalindae557@gmail.com>
2024-10-21 03:21:51 +0300

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css"; // Опционально, если стили будут использоваться
+
+ReactDOM.render(<App />, document.getElementById("root"));
